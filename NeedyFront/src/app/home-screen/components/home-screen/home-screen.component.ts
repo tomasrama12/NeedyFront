0f837d09578ea2902ec0cf71bc367c1c0959ed82
carolgsomnio <cglass@somniosoftware.com>
2023-06-09 18:34:23 -0300

    initial homescreen

---
+++ NeedyFront/src/app/home-screen/components/home-screen/home-screen.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home-screen',
+  templateUrl: './home-screen.component.html',
+  styleUrls: ['./home-screen.component.css']
+})
+export class HomeScreenComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
