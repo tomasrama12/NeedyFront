85b5330c21903e2875249e92fd4fb72249a161e5
carolgsomnio <cglass@somniosoftware.com>
2023-06-10 18:50:15 -0300

    agregue rating profile

--- NeedyFront/src/app/helper/helper.module.ts
+++ NeedyFront/src/app/helper/helper.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { CoreModule } from '../core/core.module';
 import { HelperPageComponent } from './components/helper-page/helper-page.component';
 import { HelperRoutingModule } from './helper-routing.module';
+import { HelperRatingPageComponent } from './components/helper-rating-page/helper-rating-page.component';
 
 
 @NgModule({
   declarations: [
-    HelperPageComponent
+    HelperPageComponent,
+    HelperRatingPageComponent
   ],
   imports: [
     CommonModule,
