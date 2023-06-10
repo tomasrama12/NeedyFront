df44972d18141b776ec50869f723a39616735af9
carolgsomnio <cglass@somniosoftware.com>
2023-06-10 19:56:13 -0300

    agregue la nav bar

--- NeedyFront/src/app/home/home.module.ts
+++ NeedyFront/src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { HomeRoutingModule } from './home-routing.module';
 
@@ -13,7 +14,8 @@ import { HomeRoutingModule } from './home-routing.module';
   imports: [
     CommonModule,
     CoreModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
