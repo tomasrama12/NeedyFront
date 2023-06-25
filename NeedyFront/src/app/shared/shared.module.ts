adab6185ca9c2e7f6c89909fd6f5f49bacab85c3
Maximiliano Benítez <maximiliano.benitez@correo.ucu.edu.uy>
2023-06-25 17:22:44 -0300

    stars

--- NeedyFront/src/app/shared/shared.module.ts
+++ NeedyFront/src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { CoreModule } from '../core/core.module';
+import { NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NeedCardComponent } from './components/need-card/need-card.component';
 import { AlertComponent } from './components/alert/alert.component';
+import { StarsComponent } from './components/stars/stars.component';
 
 
 @NgModule({
@@ -16,17 +18,20 @@ import { AlertComponent } from './components/alert/alert.component';
     HeaderComponent,
     NeedCardComponent,
     AlertComponent,
+    StarsComponent,
   ],
   imports: [
     CommonModule,
     CoreModule,
+    NgbRatingModule,
     RouterModule
   ],
   exports: [
     AlertComponent,
-    NavbarComponent,
     HeaderComponent,
-    NeedCardComponent
+    NavbarComponent,
+    NeedCardComponent,
+    StarsComponent
   ]
 })
 export class SharedModule { }
